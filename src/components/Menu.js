42d3613351b9803b538eab1f756703b3e070a8da
erikamats <erikamats@hotmail.com>
2018-08-23 22:36:52 -0500

    more responsive menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -15,9 +15,10 @@ class Menu extends React.Component {
   render() {
     return (
       <div id="header" className="fade fade_ani ">
+      
         <div className="h1 hideme2">
           <a href="/">
-            <h1 >Erika Matsumoto</h1>
+            <h1>Erika Matsumoto</h1>
           </a>
         </div>
 
