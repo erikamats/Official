d18278c1787d13109ad7040b519f361ba36a2b0e
erikamats <erikamats@hotmail.com>
2018-09-06 16:56:43 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -5,28 +5,20 @@ import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
-import Menu from "./components/Menu"
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
-          <BrowserRouter>
-          
-            <Switch>
-              <Menu/>
-              <div id="container">
-              <Route exact path="/" component={Home} />
-              <Route exact path="/work" component={Work} />
-              <Route exact path="/contact" component={Contact} />
-              <Route component={NotFound} />
-              </div>
-            </Switch>
-          </BrowserRouter>
-        </div>
-     
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/work" component={Work} />
+            <Route exact path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,70 @@
 import React from "react";
 import { FaGithubSquare, FaInstagram, FaLinkedinIn } from "react-icons/fa";
-// import Menu from "../components/Menu";
+import Menu from "../components/Menu"
 
-const Home = (props) => (
- <div className="homeHeader">
+const Home = () => (
+  <div className="appheader">
+    <Menu />
+    <div id="container">
+      <img
+        src={require("../images/banner.JPG")}
+        className="main-image"
+        alt="erika"
+      />
 
-<div id="main-home">
-<img
-  src={require("../images/portrait.jpg")}
-  className="main-image"
-  alt="erika"
-/>
-
-<div id="main_content">
-
-
-  <h2 className="fade fade_ani h2">
-    Full Stack Web Developer 
-    <br /> aspiring to further advance 
-    <br /> creative design skills & target
-    <br />client-focused problem solving.
-  </h2>
-
-  <div id="social fade fade_ani">
-    <div
-      className="fab github"
-      onClick={() => window.open("https://github.com/erikamats ")}
-    >
-      <FaGithubSquare className="exp github"/>
-    </div>
-
-    <div
-      className="fab instagram"
-      onClick={() => window.open("https://instagram./erikamats ")}
-    >
-      <FaInstagram className="exp instagram " />
-    </div>
-
-    <div
-      className="fab linkedIn"
-      onClick={() =>
-        window.open("https://www.linkedin.com/in/erikamatsumoto/")
-      }
-    >
-      <FaLinkedinIn className="linkedIn exp" />
+      <div className="block">
+        <p > Full Stack Developer</p>
+      </div>
     </div>
   </div>
-
-</div>
-</div> */}
-
-
- </div>
- 
 );
-
-// onClick={() => window.open("https://github.com/erikamats ")}
-
 export default Home;
 
+//hero
+//   <div id="main-home">
 
+//     <img
+//       src={require("../images/portrait.jpg")}
+//       className="main-image"
+//       alt="erika"
+//     />
+
+//     <div id="main_content">
+
+//       <h2 className="fade fade_ani h2">
+//         Full Stack Web Developer
+//         <br /> aspiring to further advance
+//         <br /> creative design skills & target
+//         <br />client-focused problem solving.
+//       </h2>
+
+//       <div id="social fade fade_ani">
+//         <div
+//           className="fab github"
+//           onClick={() => window.open("https://github.com/erikamats ")}
+//         >
+//           <FaGithubSquare className="exp github"/>
+//         </div>
+
+//         <div
+//           className="fab instagram"
+//           onClick={() => window.open("https://instagram./erikamats ")}
+//         >
+//           <FaInstagram className="exp instagram " />
+//         </div>
+
+//         <div
+//           className="fab linkedIn"
+//           onClick={() =>
+//             window.open("https://www.linkedin.com/in/erikamatsumoto/")
+//           }
+//         >
+//           <FaLinkedinIn className="linkedIn exp" />
+//         </div>
+//       </div>
+
+//     </div>
+//   </div>
+// );
+
+// // onClick={() => window.open("https://github.com/erikamats ")}
