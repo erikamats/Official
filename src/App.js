068aafa67add30eeef1b008cfdf41aacf694d683
erikamats <erikamats@hotmail.com>
2018-09-21 11:16:12 -0500

    merge revamp2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,186 @@
 // import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import NotFound from "./components/NotFound";
+// import ContactPage from "./components/Contact2";
+// import Footer from "./components/Footer";
+// import Menu from "./components/Menu";
 import React, { Component } from "react";
-import "./App.css";
+import * as moment from "moment";
+
 import Home from "./components/Home";
 
-import Contact from "./components/Contact";
-// import ContactPage from "./components/Contact2";
-import Footer from "./components/Footer";
-import Menu from "./components/Menu";
 import About from "./components/About";
-// import ProjectsPage from "./components/ProjectsPage";
 import MultiCarouselPage from "./components/MultiCarouselPage";
-import Work from "./components/Work";
-import "./Queries.css"
+import Contact from "./components/Contact";
+import scrollToComponent from "react-scroll-to-component-ssr";
+import { FaGithub, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+
+import "./App.css";
+import "./Queries.css";
+let now = moment().format("YYYY");
+
 
 class App extends Component {
-
+  
  
+  componentDidMount() {
+    scrollToComponent(this.Home, {
+      offset: 0,
+      align: "bottom",
+      duration: 1500,
+      ease: "inOutBounce"
+    });
+  }
+
   render() {
     return (
       <main>
-        <Menu  />
-     
-          <div className="main">
-            <Home />
-            <About />
-            <MultiCarouselPage />
-            {/* <Work/> */}
-            <Contact  />
-            <Footer />
-          </div>
+        <div id="nav">
+          <h2
+            className="navLogo"
+            onClick={() =>
+              scrollToComponent(this.Home, {
+                offset: 0,
+                align: "top",
+                duration: 1500,
+                ease: "inOutBack"
+              })
+            }
+          >
+            {" "}
+            Erika Matsumoto
+          </h2>
 
+          <div id="menu">
+            <div
+              className="menuitem"
+              onClick={() =>
+                scrollToComponent(this.About)
+              }
+            >
+              About
+            </div>
+
+            <div
+              className="menuitem"
+              onClick={() =>
+              scrollToComponent(this.MCP, {
+                offset: 300,
+                align: "top",
+                duration: 1500,
+                ease: "inOutBack"
+              })
+            }
+            >
+              Work
+            </div>
+
+            <div
+              className="menuitem"
+              onClick={() =>
+              scrollToComponent(this.GIT , {
+                offset: 1000,
+                align: "top",
+                duration: 1500,
+                ease: "inOutBack"
+              })
+            }
+            >
+              Contact
+            </div>
+          </div>
+        </div>
+
+        <div className="main">
+          <section
+            className="home"
+            ref={section => {
+              this.Home = section;
+            }}
+          >
+            <Home />
+          </section>
+          <section
+            className="about"
+            ref={section => {
+              this.About = section;
+            }}
+          >
+            <About />
+          </section>
+
+          <section
+            className="mcp"
+            ref={section => {
+              this.MCP = section;
+            }}
+          >
+            <MultiCarouselPage />
+          </section>
+
+          <section
+            className="git"
+            ref={section => {
+              (this.GIT = section);
+            }}
+          >
+            <Contact />
+          </section>
+
+          <div className="footer-container col-12 ">
+            <div className="copy-left col-4">
+              <span className="copyright">
+                Copyright © {now} Erika Matsumoto
+              </span>
+            </div>
+
+            <div className="copy-middle col-4">
+              <div
+                className="fab github"
+                onClick={() => window.open("https://github.com/erikamats ")}
+              >
+                <FaGithub className="exp github " />
+              </div>
+
+              <div
+                className="fab linkedIn"
+                onClick={() =>
+                  window.open("https://www.linkedin.com/in/erikamatsumoto/")
+                }
+              >
+                <FaLinkedinIn className="linkedIn exp" />
+              </div>
+              <div
+                className="fab instagram"
+                onClick={() =>
+                  window.open("https://www.instagram.com/maaatsiii/")
+                }
+              >
+                <FaInstagram className="exp instagram " />
+              </div>
+            </div>
+
+            <div className="copy-right col-4">
+              <span  onClick={() =>
+                scrollToComponent(this.Home )
+              }> Home</span>
+              <span  onClick={() =>
+                scrollToComponent(this.About )
+              } > About</span>
+              <span  
+              onClick={() =>
+              scrollToComponent(this.MCP, {
+                offset: -300,
+                align: "top",
+                duration: 1500,
+                ease: "inOutBack"
+              })
+            }> Work</span>
+              <span  onClick={() =>
+                scrollToComponent(this.Contact )
+              }> Contact</span>
+            </div>
+          </div>
+        </div>
       </main>
     );
   }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,104 +1,104 @@
-import React from "react";
+// import React from "react";
 
-import {
-  Link,
-  DirectLink,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller
-} from "react-scroll";
+// import {
+//   Link,
+//   DirectLink,
+//   Element,
+//   Events,
+//   animateScroll as scroll,
+//   scrollSpy,
+//   scroller
+// } from "react-scroll";
 
-class Menu extends React.Component {
+// class Menu extends React.Component {
 
-  constructor(props) {
-    super(props);
-     this.scrollToTop = this.scrollToTop.bind(this);
+//   constructor(props) {
+//     super(props);
+//      this.scrollToTop = this.scrollToTop.bind(this);
 
-  }
+//   }
 
-  componentDidMount() {
-    Events.scrollEvent.register("begin", function() {
-      console.log("begin", arguments);
-      this.props.onRef(this)
-    });
+//   componentDidMount() {
+//     Events.scrollEvent.register("begin", function() {
+//       console.log("begin", arguments);
+//       this.props.onRef(this)
+//     });
 
-    Events.scrollEvent.register("end", function() {
-      console.log("end", arguments);
-    });
+//     Events.scrollEvent.register("end", function() {
+//       console.log("end", arguments);
+//     });
 
-    scrollSpy.update();
-  }
-  scrollTo() {
-    scroller.scrollTo("scroll-to-element", {
-      duration: 800,
-      delay: 0,
-      smooth: "easeInOutQuart"
-    });
-  }
+//     scrollSpy.update();
+//   }
+//   scrollTo() {
+//     scroller.scrollTo("scroll-to-element", {
+//       duration: 800,
+//       delay: 0,
+//       smooth: "easeInOutQuart"
+//     });
+//   }
 
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove("begin");
-    Events.scrollEvent.remove("end");
-    this.props.onRef(undefined)
-  }
+//   scrollToTop() {
+//     scroll.scrollToTop();
+//   }
+//   componentWillUnmount() {
+//     Events.scrollEvent.remove("begin");
+//     Events.scrollEvent.remove("end");
+//     this.props.onRef(undefined)
+//   }
 
-  render() {
+//   render() {
  
-    return(
-      <div id="nav">
+//     return(
+//       <div id="nav">
  
-        <h2 className="navLogo"> Erika Matsumoto</h2>
+//         <h2 className="navLogo"> Erika Matsumoto</h2>
 
         
 
-        <div id="menu">
-          <div className="hideme menuitem" onClick={() => scroll.scrollTo(200)} >About</div>
+//         <div id="menu">
+//           <div className="hideme menuitem" onClick={() => scroll.scrollTo(200)} >About</div>
 
-          <div className=" menuitem" onClick={() => scroll.scrollTo(800)}>Work</div>
+//           <div className=" menuitem" onClick={() => scroll.scrollTo(800)}>Work</div>
 
-          <div className=" menuitem"
+//           <div className=" menuitem"
           
-          onClick={() => scroll.scrollTo(1500)}>Contact</div>
-        </div>
-      </div>
-    );
-  }
-}
-export default Menu;
+//           onClick={() => scroll.scrollTo(1500)}>Contact</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Menu;
 
-// state = {
-//   clicked: false
-// };
+// // state = {
+// //   clicked: false
+// // };
 
-// toggle = () => {
-//   // console.log("click detected");
-//   this.setState({
-//     clicked: !this.state.clicked
-//   });
-// };
+// // toggle = () => {
+// //   // console.log("click detected");
+// //   this.setState({
+// //     clicked: !this.state.clicked
+// //   });
+// // };
 
-  /* <div id="header" className="fade fade_ani">
+//   /* <div id="header" className="fade fade_ani">
       
-        <div className="h1 hideme2">Y654`13A  V 
-          <h1>Erika Matsumoto</h1>
-        </a>
-      </div>
+//         <div className="h1 hideme2">Y654`13A  V 
+//           <h1>Erika Matsumoto</h1>
+//         </a>
+//       </div>
 
-      <div id="menu">
-        <a href="/" className="hideme menuitem">
-          Home
-        </a>
-        <a className="menuitem" href="/work">
-          Work
-        </a>
-        <a className="menuitem" href="/contact">
-          Contact
-        </a>
-      </div>
-    </div> */
+//       <div id="menu">
+//         <a href="/" className="hideme menuitem">
+//           Home
+//         </a>
+//         <a className="menuitem" href="/work">
+//           Work
+//         </a>
+//         <a className="menuitem" href="/contact">
+//           Contact
+//         </a>
+//       </div>
+//     </div> */
 
