af702213246c285624b0d73ea2afd443d7d24b98
erikamats <erikamats@hotmail.com>
2018-09-26 12:20:08 -0500

    about

--- src/components/About.js
+++ src/components/About.js
@@ -3,18 +3,26 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-container col-12">
-
         <div className="col-2 iAm-label">
-        <p className="iAm-stack">I am <span className="
-        dots">...</span></p>
-          <div className="iAm-block" />      
+          <p className="iAm-stack">
+            I am{" "}
+            <span
+              className="
+        dots"
+            >
+              ...
+            </span>
+          </p>
+          <div className="iAm-block" />
         </div>
 
         <p className="iAm-merp col-9">
+        I am a Full Stack Web Developer living in Austin, Texas. I recently took a proud leap into web development to pursue something that I genuinely enjoy and I am thrilled about 
+        </p>
+        <p>
           {" "}
-          A Full Stack Web Developer living in Austin, Texas. I recently took a
-          leap into web development and I am very happy to have taken the initiative to pursue something that
-          I genuinly enjoy!{" "}
+          I am a problem solver and people lover. I always welcome challenges because I am bound to learn something I didn't know before and I thrive in environments where I can continue to grow. I am
+          passionate about leading by example and sharing my knowledge and best practices with others. I take pride in the quality of all of my endeavors and always put my best efforts into doing things right which have led to my career achievements.
         </p>
         {/* <div className="col-2 iAm-label"/> */}
       </div>
