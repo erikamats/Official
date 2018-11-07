76b28179970c437ec164e8db228bebd96bc7b370
erikamats <erikamats@hotmail.com>
2018-11-06 22:18:57 -0600

    source button name △

--- src/components/MultiCarouselPage.js
+++ src/components/MultiCarouselPage.js
@@ -124,7 +124,7 @@ class MultiCarouselPage extends Component {
                     <button className="btn btn-md ">
                       <a href={proj1.github} target="_blank">
                         <Fa icon="github" />
-                        <span className="d-sm  d-md">Github</span>
+                        <span className="d-sm  d-md">Source</span>
                       </a>
                     </button>
                   </CardBody>
